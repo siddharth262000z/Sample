3a3c82461e2d4504d68ab7a4c864f7561a9a527f
Siddharth <sid262000z@gmail.com>
2025-09-02 12:05:12 +0530

    ww

--- eg.js
+++ eg.js
@@ -2,15 +2,25 @@
 setTimeout(()=>{
     console.log('eeeee');
 })
+<<<<<<< Updated upstream
 console.log(`changed main`);  //main
  //feature
+=======
+console.log('changed in feature');
+
+>>>>>>> Stashed changes
 setTimeout(()=>{
     console.log('this is it');  
 },1000)
+<<<<<<< Updated upstream
 console.log(`changed main`);   //main
+=======
+console.log('changed in feature')
+>>>>>>> Stashed changes
 setTimeout(()=>{
     console.log('ssssssswwwww');  
 })
+console.log('changed in feature')
 setTimeout(()=>{
     console.log('ssssssaaaaaaaaswwwww');
 })
