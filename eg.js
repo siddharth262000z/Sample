8e200a4270861f5d5c4f6b98b58af079c85d0366
Siddharth <sid262000z@gmail.com>
2025-09-02 12:44:38 +0530

    qqq

--- eg.js
+++ eg.js
@@ -1,21 +1,14 @@
 setTimeout(()=>{
     console.log('eeeee');
 })
-<<<<<<< Updated upstream
-console.log('changed in main'); //main
-console.log('changed in main'); //main
+console.log(`changed main`);  //main
+console.log('changed in feature');  //feature
+
 setTimeout(()=>{
     console.log('this is it');  
 },1000)
 console.log(`changed main`);   //main
-=======
-console.log('changed in feature'); //feature
-console.log('changed in feature'); //feature
-setTimeout(()=>{
-    console.log('this is it');  
-},1000)
-console.log(`changed feature`);   //feature
->>>>>>> Stashed changes
+console.log('changed in feature')
 setTimeout(()=>{
     console.log('ssssssswwwww');  
 })
