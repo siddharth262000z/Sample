282aa1a8db35d33a9d0851acd1a0a0fca782f04c
Siddharth <sid262000z@gmail.com>
2025-09-02 11:45:51 +0530

    new feature

--- eg.js
+++ eg.js
@@ -3,15 +3,13 @@ setTimeout(()=>{
     console.log('eeeee');
 })
 console.log('changed in feature');  //feature
-console.log('changed in main'); //main
 setTimeout(()=>{
     console.log('this is it');  
 },1000)
-console.log('changed in main');  //main
+console.log('changed in feature');   //feature
 setTimeout(()=>{
     console.log('ssssssswwwww');  
 })
-console.log('changed in feature');  //feature
 setTimeout(()=>{
     console.log('ssssssaaaaaaaaswwwww');
 })
