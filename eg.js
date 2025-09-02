ca1d97f1985ae8fdc636b4410dbc91790fde8bab
Siddharth <sid262000z@gmail.com>
2025-09-02 11:59:55 +0530

    yes

--- eg.js
+++ eg.js
@@ -2,11 +2,12 @@
 setTimeout(()=>{
     console.log('eeeee');
 })
-console.log('changed in feature');  //feature
+console.log(`changed main`);  //main
+ //feature
 setTimeout(()=>{
     console.log('this is it');  
 },1000)
-console.log('changed in feature');   //feature
+console.log(`changed main`);   //main
 setTimeout(()=>{
     console.log('ssssssswwwww');  
 })
