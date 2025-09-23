d800369c712cd9d827ddcd5a61adcf7ea7019015
Siddharth <sid262000z@gmail.com>
2025-09-23 10:35:10 +0530

    q

--- today.js
+++ today.js
@@ -2,7 +2,7 @@ console.log('1');
 async function firstPop() {
     console.log('wow mainq');
     console.log('lll model');
-    console.log(feature);
+    console.log(featureqq);
     console.log('chow mainq'); //main
     
     let k = new Promise((res, rej) => {
